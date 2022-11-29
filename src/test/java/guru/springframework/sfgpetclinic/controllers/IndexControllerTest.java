53d919c726417c8ce52088c580056163a44922f0
Radek <rdwwojcik@gmail.com>
2022-11-29 08:59:03 +0100

    Assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,101 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+class IndexControllerTest {
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new IndexController();
+    }
+
+    @DisplayName("Test proper view name is returned for index page")
+    @Test
+    void index() {
+        assertEquals("index", controller.index());
+        assertEquals("index", controller.index(), "Wrong view returned");
+
+        assertEquals("index", controller.index(), () -> "Another Expensive Message " +
+                "Make me only if you have to");
+
+        assertThat(controller.index()).isEqualTo("index");
+    }
+
+    @Test
+    @DisplayName("Test exception")
+    void oopsHandler() {
+        assertThrows(ValueNotFoundException.class, () -> {
+            controller.oopsHandler();
+        });
+    }
+
+    @Disabled("Demo of timeout")
+    @Test
+    void testTimeOut() {
+        assertTimeout(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+            System.out.println("I got here");
+        });
+    }
+
+    @Disabled("Demo of timeout")
+    @Test
+    void testTimeOutPrempt() {
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+            System.out.println("I got here 12312312412");
+        });
+    }
+
+    @Test
+    void testAssumptionTrue() {
+        assumeTrue("GURU".equalsIgnoreCase(System.getenv("GURU_RUNTIME")));
+    }
+
+    @Test
+    void testAssumptionTrueAssumptionsIsTrue() {
+        assumeTrue("GURU".equalsIgnoreCase("GURU"));
+    }
+
+    @EnabledOnOs(OS.MAC)
+    @Test
+    void testMeOnMacOS() {
+    }
+
+    @EnabledOnOs(OS.WINDOWS)
+    @Test
+    void testMeOnWindows() {
+    }
+
+    @EnabledOnJre(JRE.JAVA_8)
+    @Test
+    void testMeOnJava8() {
+    }
+
+    @EnabledOnJre(JRE.JAVA_11)
+    @Test
+    void testMeOnJava11() {
+    }
+
+    @EnabledIfEnvironmentVariable(named = "USERNAME", matches = "Radek")
+    @Test
+    void testIfUserRadek() {
+    }
+
+    @EnabledIfEnvironmentVariable(named = "USERNAME", matches = "fred")
+    @Test
+    void testIfUserFred() {
+    }
+}
---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,30 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void grouppedAssertions() {
+        //given
+        Person person = new Person(1l, "Joe", "Buck");
+        //then
+
+        assertAll("Test Props Set",
+                () -> assertEquals("Joe", person.getFirstName()),
+                () -> assertEquals("Buck", person.getLastName()));
+    }
+
+    @Test
+    void grouppedAssertionsMsgs() {
+        //given
+        Person person = new Person(1l, "Joe", "Buck");
+        //then
+
+        assertAll("Test Props Set",
+                () -> assertEquals("Joe", person.getFirstName(), "First name failed"),
+                () -> assertEquals("Buck", person.getLastName(), "Last name failed"));
+    }
+}
